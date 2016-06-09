0416dfdf1251d8dcde471e096ff22ce2790fa44c
Kim Egenvall <egenvall.kim@gmail.com>
2016-06-09 22:03:50 +0200

    Reminder, next step:_ Routing

--- app/app.component.ts
+++ app/app.component.ts
@@ -75,6 +75,7 @@ export class AppComponent implements OnInit {
   constructor(private heroService: HeroService) { }
   getHeroes() {
     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    
   }
   ngOnInit() {
     this.getHeroes();
